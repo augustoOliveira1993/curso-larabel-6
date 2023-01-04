32c4c94c4a5de002a601a692e48d4359785ff012
Augusto Silva <gutim160@gmail.com>
2023-01-04 10:03:27 -0300

    refactore: finalizado modulo de rotas laravel

---
+++ app/Http/Controllers/Admin/TesteController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TesteController extends Controller
+{
+    public function teste()
+    {
+        return 'Teste Controller';
+    }
+
+    public function teste2()
+    {
+        return 'Teste 2222 Controller';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,63 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+Route::redirect('redirect1', '/redirect2');
+
+//Route::get('redirect1', function () {
+//    return redirect('/redirect2');
+//});
+
+Route::get('redirect2', function () {
+    return redirect()->away('https://www.google.com');
+});
+
+Route::get('produtos/{idProduto?}', function ($idProduto = '') {
+    return "Produto {$idProduto}";
+});
+
+Route::get('categories/{flag}', function ($flag) {
+    return 'flag: ' . $flag;
+});
+
+Route::match(['get', 'post'], '/match', function () {
+    return 'match';
+});
+
+Route::get('/contato', function () {
+    return view('contact');
+});
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::view('/view', 'welcome', ['id' => 'teste']);
+
+Route::get('/nome-url', function () {
+    return 'Hey hey hey';
+})->name('url.name');
+
+Route::get('/redirect3', function () {
+    return redirect()->route('url.name');
+});
+
+
+Route::get('/login', function () {
+    return 'Login';
+})->name('login');
+
+
+Route::group([
+    'middleware' => [],
+    'prefix' => 'admin',
+    'namespace' => 'App\Http\Controllers\Admin',
+    'name' => 'admin'
+], function () {
+    Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+    Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+    Route::get('/produtos', 'TesteController@teste')->name('products');
+    Route::get('/', function () {
+        return redirect()->route('admin.dashboard');
+    })->name('home');
 });
