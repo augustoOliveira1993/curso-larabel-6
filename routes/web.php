674e564b47954b0aabf86d7a2826fde7fffb2e69
Augusto Silva <gutim160@gmail.com>
2023-01-04 10:50:44 -0300

    refactore: padrão resource do laravel para facilitar na criação de rotas

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,13 @@ use App\Http\Controllers\ProductController;
 Route::get('/login', function () {
     return 'Login';
 })->name('login');
-
-Route::delete('products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
-Route::put('products/{id}', [ProductController::class, 'update'])->name('products.update');
-Route::get('products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
-Route::get('products/{id}', [ProductController::class, 'show'])->name('products.show');
-Route::get('products', [ProductController::class, 'index'])->name('products.index');
-Route::post('products', [ProductController::class, 'store'])->name('products.store');
+/*
+    Route::delete('products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+    Route::put('products/{id}', [ProductController::class, 'update'])->name('products.update');
+    Route::get('products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::get('products/{id}', [ProductController::class, 'show'])->name('products.show');
+    Route::get('products', [ProductController::class, 'index'])->name('products.index');
+    Route::post('products', [ProductController::class, 'store'])->name('products.store');
+*/
+Route::resource('products', ProductController::class);
